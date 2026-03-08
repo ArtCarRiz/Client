72c8d42c9fa1a275b0888b47e0a6e6cbfed30bf8
ArturoC <arturo.c.rizo10@gmail.com>
2026-03-08 17:27:57 -0600

    cliente 2.0

--- src/main/java/com/DIGIS01/ACardenasProgramacionNCapas/ML/Result.java
+++ src/main/java/com/DIGIS01/ACardenasProgramacionNCapas/ML/Result.java
@@ -2,12 +2,12 @@ package com.DIGIS01.ACardenasProgramacionNCapas.ML;
 
 import java.util.List;
 
-public class Result {
+public class  Result <T> {
 
     public boolean correct;
     public String errorMessage;
     public Exception ex;
-    public Object object; //devolver de un solo registro
-    public List<Object> objects; //devolver de mas de un registro 
+    public T object; //devolver de un solo registro
+    public List<T> objects; //devolver de mas de un registro 
 
 }
