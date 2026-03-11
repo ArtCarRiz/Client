3d94c6e4baf6b7b7a2a3408b9718bc4e8ad3ffac
digis <digis@LAPTOP-AD7LFKO5>
2026-03-10 18:53:50 -0600

    cliente 10032026

--- src/main/java/com/DIGIS01/ACardenasProgramacionNCapas/Controller/UsuarioController.java
+++ src/main/java/com/DIGIS01/ACardenasProgramacionNCapas/Controller/UsuarioController.java
@@ -4,10 +4,13 @@
  */
 package com.DIGIS01.ACardenasProgramacionNCapas.Controller;
 
+import com.DIGIS01.ACardenasProgramacionNCapas.ML.Direccion;
 import com.DIGIS01.ACardenasProgramacionNCapas.ML.Pais;
 import com.DIGIS01.ACardenasProgramacionNCapas.ML.Result;
 import com.DIGIS01.ACardenasProgramacionNCapas.ML.Usuario;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -21,12 +24,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import tools.jackson.databind.ObjectMapper;
 //import org.apache.commons.io.FileUtils;
 
 /**
@@ -37,7 +44,8 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("usuario")
 public class UsuarioController {
 
-    private static String rutaBase = "http://127.0.0.1:8080";
+//    private static String rutaBase = "http://127.0.0.1:8080";
+    private static String rutaBase = "http://localhost:8080";
 
     @GetMapping
     public String Index(Model model) {
@@ -139,12 +147,12 @@ public class UsuarioController {
             }
 
             HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-            
+
             ResponseEntity<Result> response = restTemplate.exchange(rutaBase + "/api/usuario",
                     HttpMethod.POST,
                     requestEntity,
                     Result.class);
-            
+
             result.correct = true;
             if (result.correct) {
                 return "redirect:/usuario";
@@ -161,4 +169,164 @@ public class UsuarioController {
         return "redirect:/usuario";
     }
 
+    @GetMapping("/delete/{IdUsuario}")
+    public String DeteleUsuario(@PathVariable("IdUsuario") int identificador, RedirectAttributes redirectAttributes) {
+        Result result = new Result();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Result> responseBorrar = restTemplate.exchange(rutaBase + "/api/usuario/Delete/Usuario/{identificador}",
+                HttpMethod.DELETE,
+                null,
+                new ParameterizedTypeReference<Result>() {
+        },
+                identificador);
+
+        result.correct = true;
+
+        if (result.correct) {
+            redirectAttributes.addFlashAttribute("mensaje", "Borrado exitosamente");
+            return "redirect:/usuario";
+        } else {
+            return "redirect:/usuario";
+        }
+
+    }
+
+    @GetMapping("/details/{IdUsuario}")
+    public String Details(@PathVariable("IdUsuario") int identificador, Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+
+            ResponseEntity<Result<Usuario>> responseGetById = restTemplate.exchange(rutaBase + "/api/usuario/{identificador}",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<Result<Usuario>>() {
+            },
+                    identificador);
+
+            if (responseGetById.getStatusCode().value() == 200) { // OK
+
+                Result result = responseGetById.getBody();
+
+                model.addAttribute("usuario", result.object);
+                model.addAttribute("direccion", new Direccion());
+                //roles
+                ResponseEntity<Result> responseRoles = restTemplate.exchange(rutaBase + "/api/usuario/Rol",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<Result>() {
+                });
+                model.addAttribute("roles", responseRoles.getBody().objects);
+
+                //roles
+                ResponseEntity<Result> responsePaises = restTemplate.exchange(rutaBase + "/api/usuario/Pais",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<Result>() {
+                });
+                model.addAttribute("paises", responsePaises.getBody().objects);
+
+                Result<Usuario> usuario = responseGetById.getBody();
+
+                int idPais = usuario.object.Direcciones.get(0).colonia.municipio.estado.pais.getIdPais();
+                int idEstado = usuario.object.Direcciones.get(0).colonia.municipio.estado.getIdEstado();
+                int idMunicipio = usuario.object.Direcciones.get(0).colonia.municipio.getIdMunicipio();
+                int idColonia = usuario.object.Direcciones.get(0).colonia.getIdColonia();
+
+                if (idEstado != 0) {
+                    //guardo el valor
+                    model.addAttribute("estados", restTemplate.getForObject(rutaBase + "/api/usuario/Estado/{identificador}}", List.class, idPais));
+                    if (idMunicipio != 0) {
+                        //guardo el valor
+                        model.addAttribute("municipios", restTemplate.getForObject(rutaBase + "/api/usuario/Municipio/{identificador}", List.class, idEstado));
+                        if (idColonia != 0) {
+                            //guardo el valor
+                            model.addAttribute("colonias", restTemplate.getForObject(rutaBase + "/api/usuario/Colonia/{identificador}", List.class, idMunicipio));
+
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error en details: " + e.getLocalizedMessage());
+        }
+
+        return "details";
+    }
+
+    @PostMapping("/update/{IdUsuario}")
+    public String UpdateUsuario(@ModelAttribute("usuario") Usuario usuario, @PathVariable("IdUsuario") int identificador, Model model) {
+
+        Result result = new Result();
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            ResponseEntity<Result<Usuario>> responseGetById = restTemplate.exchange(rutaBase + "/api/usuario/{identificador}",
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<Result<Usuario>>() {
+            },
+                    identificador);
+
+            result = responseGetById.getBody();
+
+            if (result.correct) {
+                Usuario usuarioanterior = (Usuario) result.object;
+                usuario.setImagen(usuarioanterior.getImagen());
+            }
+            
+            ResponseEntity<Result> responseUpdate = restTemplate.exchange(rutaBase + "/api/usuario",
+                    HttpMethod.PUT,
+                     new HttpEntity<>(usuario),
+                    new ParameterizedTypeReference<Result>() {
+            });
+            
+            result = responseUpdate.getBody();
+            
+            if (!result.correct) {
+                 return "redirect:/usuario/details/" + identificador;
+            }
+
+        } catch (Exception e) {
+            result.correct = false;
+            result.errorMessage = e.getLocalizedMessage();
+            result.ex = e;
+        }
+
+        return "redirect:/usuario/details/" + identificador;
+    }
+    
+    @PostMapping("update/{IdDireccion}/{IdUsuario}")
+    public String UpdateDireccion (@ModelAttribute("direccion") Direccion direccion, @PathVariable("IdDireccion") int IdDireccion, @PathVariable("IdUsuario") int IdUsuario, RedirectAttributes redirectAttributes){
+        Result result = new Result();
+        RestTemplate restTemplate = new RestTemplate();
+        direccion.setIdDireccion(IdDireccion);
+        try {
+            
+            ResponseEntity<Result> responseUpdateDireccion = restTemplate.exchange(rutaBase + "/api/usuario/Direccion", 
+                    HttpMethod.PUT,
+                    new HttpEntity<>(direccion),
+                    new ParameterizedTypeReference<Result>() {});
+            
+            result = responseUpdateDireccion.getBody();
+            
+            if (result.correct) {
+                redirectAttributes.addFlashAttribute("mensaje", "Dirección actualizada correctamente");
+            }else{
+                redirectAttributes.addFlashAttribute("error", "Error al actualizar: " + result.errorMessage);
+            }
+            
+            
+        } catch (Exception e) {
+            result.correct = false;
+            result.errorMessage = e.getLocalizedMessage();
+            result.ex = e;
+        }
+        
+        
+        return "redirect:/usuario/details/" + IdUsuario;
+    }
+
+    
 }
